3f6be2f17ca80904470e1b795cef551628fc262a
Argin-hub <mukushev77.89@mail.ru>
2020-01-17 16:51:20 +0600

    Первая дз

---
+++ src/com/ucheba_one/Main.java
@@ -0,0 +1,41 @@
+package com.ucheba_one;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.print("Java ");
+        System.out.println("уже здесь!");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Аргумент " + i +
+                    " = " + args[i]);
+
+            int items = 4;
+            double salary = 1.234e3;
+            float itemCost = 11.0f;
+            // int i = 0xFd45, k$;
+            // double _interestRate;
+
+
+
+
+        }
+
+        float f1 = 1.2345f;
+        double d1 = f1;
+        double d2 = 1.2345;
+
+        System.out.println("f1 - " + f1);
+        System.out.println("d1 - " + d1);
+        System.out.println("--------------------");
+
+        System.out.printf("f1 = %.16f\n", f1);
+        System.out.printf("d2 = %.16f\n", d2);
+
+    }
+
+}
+
+
